d1e15295655088ede1e92944d6764766fed08a32
Carlos Rodriguez <carlos@s8f.org>
2018-06-01 01:35:10 -0700

    fix deprecation notice

--- launcher/get-mongo.js
+++ launcher/get-mongo.js
@@ -26,7 +26,7 @@ module.exports = function getMongo (cb) {
         (mongo_conf.replica_set ? '&replicaSet=' + mongo_conf.replica_set : '' ) +
         (auth_mechanism ? '&authMechanism=' + auth_mechanism : '' )
     }
-    require('mongodb').MongoClient.connect(connection_str, function (err, client) {
+    require('mongodb').MongoClient.connect(connection_str, {useNewUrlParser: true}, function (err, client) {
       if (err) {
         return cb(new Error('Failed to connect to MongoDB. Check your bot18.config.js and try again.'))
       }
